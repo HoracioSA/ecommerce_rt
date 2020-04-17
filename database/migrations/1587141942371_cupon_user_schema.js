e87269001a4241b8e01c251b7b63721648515770
Horacio <Horacio@Ilias-MacBook-Pro.local>
2020-04-17 20:04:14 +0300

    Coupon user relationship

---
+++ database/migrations/1587141942371_cupon_user_schema.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CuponUserSchema extends Schema {
+  up () {
+    this.create('cupon_users', (table) => {
+      table.increments()
+      table.integer('cupon_id').unsigned()
+      table.integer('user_id').unsigned()
+
+      table.timestamps()
+      table
+      .foreign('cupon_id')
+      .references('id')
+      .inTable('cupons')
+      .onDelete('cascade')
+
+      table
+      .foreign('user_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('cascade')
+
+    })
+  }
+
+  down () {
+    this.drop('cupon_users')
+  }
+}
+
+module.exports = CuponUserSchema
