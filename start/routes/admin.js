eec374c77dfc1d8d9563cb2519e34eaf08cb58a5
Horacio <Horacio@Ilias-MacBook-Pro.local>
2020-04-24 17:52:26 +0300

    Admin and Auth routes controllers

---
+++ start/routes/admin.js
@@ -0,0 +1,25 @@
+'use strict'
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+Route.group(()=>{
+    /**
+     * Categories Ressurce routes
+     */
+    Route.resource('categories', 'CategoryController').apiOnly()
+    /**
+     * Products Ressurce routes
+     */
+    Route.resource('products', 'ProductController').apiOnly()
+/**
+     * Products Ressurce routes
+     */
+    Route.resource('coupons', 'CouponController').apiOnly()
+    /**
+     * Products Ressurce routes
+     */
+    Route.resource('orders', 'OrderController').apiOnly()
+
+    Route.resource('users', 'UserController').apiOnly()
+    Route.resource('images', 'ImageController').apiOnly()
+
+}).prefix('admin/v1').namespace('Admin')
