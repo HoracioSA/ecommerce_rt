99f51d0cc5865bd9ea769d2b178cff42833470d0
Horacio <Horacio@Ilias-MacBook-Pro.local>
2020-05-02 20:28:14 +0300

    Updated Buffer for Random String

--- app/Helpers/index.js
+++ app/Helpers/index.js
@@ -11,11 +11,14 @@ const Helpers= use('Helpers')
  const str_random = async (length = 40)=>{
      let string =''
      let len = string.length
-     if (len > length) {
+     if (len < length) {
          let size = length - len
-         let bytes = await crypto.radomBytes(size)
-         let buffer = new Buffer(bytes)
-         string += buffer.toString('base64').replace(/[^a-zA-z0-9]/g, '').substr(0, size)
+         let bytes = await crypto.randomBytes(size)
+         let buffer = Buffer.from(bytes)
+         string += buffer
+         .toString('base64')
+         .replace(/[^a-zA-Z0-9]/g, '')
+         .substr(0, size)
 
      }
      return string
